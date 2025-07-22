f3461436440ad27c2fef41dfeef3a71927d3aec3
Eyinade Peter <itemize6@gmail.com>
2025-07-22 14:12:04 +0100

    first commit

--- components/hero.tsx
+++ components/hero.tsx
@@ -158,7 +158,7 @@ export default function Hero() {
               </Button>
             </div>
           </motion.div>
-
+          <div></div>
           <motion.div
             initial={{ opacity: 0, scale: 0.9 }}
             animate={{ opacity: 1, scale: 1 }}
